654f83574529fd1d292313eff749e565a3820c63
Pascal Sommer <p@pascalsommer.ch>
2016-11-16 13:48:07 +0100

    Core: create fifo dir, check io files

--- core.sh
+++ core.sh
@@ -1,6 +1,23 @@
 # the streamgraph executable core
+# Pascal Sommer, November 2016
+
+# most of this will probably be wrapped in functions so
+# that it can be called from the compiled streamgraph
+# program.
+
+# sg program will set variable $program_name with .sg exension
 
 set -u # throw an error if unset variables are used
+
+# create the fifo directory
+
+fifo_directory="/tmp/fifos"
+if [ ! -e "$fifo_directory" ]; then
+    mkdir "$fifo_directory"
+elif [ ! -d "$fifo_directory" ]; then
+    echo "Can't create $fifo_directory because file exists" >&2
+fi  
+
 
 # get the arguments
 
@@ -9,8 +26,8 @@ output_files=() # -o option arguments
 
 while [ $# -ge 1 ]; do
 
-    # this can read unnamed arguments and option arguments
-    # even if they're not in order
+    # this construct can read unnamed arguments and option
+    # arguments even if they're not in order
 
     while [ $# -ge 1 ] && [ ${1:0:1} != "-" ]; do
 	input_files+=($1)
@@ -25,11 +42,11 @@ while [ $# -ge 1 ]; do
 		output_files+=($OPTARG)
 		;;
 	    \?)
-		echo -e "ERROR: Unknown option -$OPTARG\n" >&2
+		echo "ERROR: Unknown option -$OPTARG\n" >&2
 		exit 1
 		;;
 	    :)
-		echo -e "Error: Option -$OPTARG requires an option argument\n" >&2
+		echo "Error: Option -$OPTARG requires an option argument\n" >&2
 		exit 1
 		;;
 	esac
@@ -39,4 +56,29 @@ while [ $# -ge 1 ]; do
 
 done
 
+# these will be set by the compiled script, for now this is just a placeholder
+nr_input_files=2
+nr_output_files=0
 
+function check_io_files{
+    if [ $nr_input_files -ne ${input_files[@]} ]; then
+	echo "ERROR: incorrect number of input files given!" >&2
+	echo "    Expected: $nr_input_files files" >&2
+	echo "    Given: ${input_files[@]} files" >&2
+	exit 1
+    fi
+    
+    if [ $nr_output_files -ne ${output_files[@]} ]; then
+	echo "ERROR: incorrect number of output files given!" >&2
+	echo "    Expected: $nr_output_files files" >&2
+	echo "    Given: ${output_files[@]} files" >&2
+	exit 1
+    fi
+
+    for f in ("${input_files[@]}" "${output_files[@]}"); do
+	if [ ! -f "$f" ]; then
+	    echo "ERROR: file $f does not exist!" >&2
+	    exit 1
+	fi
+    done
+}
