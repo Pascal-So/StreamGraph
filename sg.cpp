26a17dbf38f1b6e013e81aa4fd17439054be9b3d
Pascal Sommer <p@pascalsommer.ch>
2016-11-22 12:22:53 +0100

    Moved scanner to new file

--- sg.cpp
+++ sg.cpp
@@ -9,19 +9,6 @@
 
 
 
-// some utility functions
-
-bool is_whitespace(char c){
-    return isspace(c);
-}
-
-bool is_not_newline(char c){
-    return (c!='\n' && c!='\r');
-}
-
-bool is_alphanumeric(char c){
-    return isalnum(c);
-}
 
 template <typename T>
 void vector_append(std::vector<T> &v1, const std::vector<T> &v2){
@@ -29,171 +16,6 @@ void vector_append(std::vector<T> &v1, const std::vector<T> &v2){
 }
 
 
-class Scanner{
-    // The Scanner class doesn't do the lexing, but is the interface for the
-    // actual lexer to the file. It keeps track of the line and character
-    // number, and lets the lexer fetch words or lines, and provides a simple
-    // form of backtracking.
-    std::string buffer;
-    std::ifstream infile;
-    std::string file_path;
-    // has filestream reached eof? if this is true, buffer could still be non-
-    // empty.
-    bool eof;
-    
-    int line_number;
-    int line_character_number;
-
-    bool fill_buffer(int len){	
-        while (buffer.size() < len){
-	    if(infile.eof()){
-		return false;
-	    }
-
-	    char next;
-	    if( !infile.get(next)){
-		eof = true;
-		return false;
-	    }else{
-		buffer+=next;
-	    }
-	}
-	return true;
-    }
-
-
-    // return substring from buffer and then remove it. If len is
-    // greater than the buffer length, the entire buffer will be
-    // returned quietly. len -1 will return the entire buffer as
-    // well.
-    // this function updates the line and character number, thus
-    // every function that clears something from the buffer must
-    // do such through this function.
-    std::string clear_from_buffer(int len){
-	if(len == -1){
-	    len = buffer.size();
-	}
-	len = std::min(len, (int)buffer.size());	
-	std::string out = buffer.substr(0, len);
-	for(auto c:out){
-	    if(is_not_newline(c)){
-		++line_character_number;
-	    }else{
-		line_character_number = 0;
-		++line_number;
-	    }
-	}
-	buffer.erase(buffer.begin(), buffer.begin()+len);
-	return out;
-    }
-
-    // consumes the input while the next character fulfills (*cond).
-    // usually leaves a single character in the buffer, unless eof
-    // was reached.
-    std::string get_while(bool (*cond)(char)){
-	
-	for(int i = 0; i < buffer.size(); ++i){
-	    if( ! (*cond)(buffer[i])){
-		return clear_from_buffer(i);
-	    }
-	}
-
-	std::string out = clear_from_buffer(-1);
-
-	for(;;){
-	    if( ! fill_buffer(1)) break;
-	    if( ! (*cond)(buffer[0])) break;
-	    out += clear_from_buffer(1);
-	}
-
-	return out;
-    }
-
-
-    std::string peek_str(int len){
-	if( ! fill_buffer(len)){
-	    // return as much as possible if eof is reached
-	    len = buffer.size();
-	}
-
-	std::string out = buffer.substr(0, len);
-	return out;
-    }
-
-    std::string get_str(int len){
-	std::string out = peek_str(len);
-	// this might be shorter than the original len
-	len = out.size();
-	clear_from_buffer(len);
-	return out;
-    }
-
-    
-
-public:
-    bool reached_eof();
-    bool match_string(std::string pattern);
-    void skip_whitespace();
-    std::string get_alphanum();
-    std::string get_rest_of_line();
-    std::string get_position();
-    
-    Scanner(std::string file_path): file_path(file_path)
-    {
-	infile.open(file_path, std::ifstream::in);
-	line_number = 1;
-	line_character_number = 0;
-	buffer = "";
-	eof = false;
-    }
-};
-
-void Scanner::skip_whitespace(){
-    get_while(&is_whitespace);
-}
-
-// is eof reached?
-bool Scanner::reached_eof(){
-    return buffer.empty() && eof;
-}
-
-// if string matches, consume input, otherwise don't
-// consume anything. Skips any leading whitespace.
-bool Scanner::match_string(std::string pattern){
-    skip_whitespace();
-    std::string in = peek_str(pattern.size());
-
-    if(in.size() < pattern.size()){
-	return false;
-    }
-
-    if( in == pattern ){
-	// if string matches, consume it.
-	get_str(pattern.size());
-	return true;
-    }else{
-	return false;
-    }
-}
-
-// consumes word unless EOF. Returns without leading
-// or trailing whitespace.
-std::string Scanner::get_alphanum(){
-    skip_whitespace();
-    return get_while(&is_alphanumeric);
-}
-
-// get until newline, and consume input.
-std::string Scanner::get_rest_of_line(){
-    return get_while(&is_not_newline);
-}
-
-// return the file path, line number and character number
-std::string Scanner::get_position(){
-    return file_path + ", " +
-	std::to_string(line_number) + ":" +
-	std::to_string(line_character_number);
-}
 
 
 
@@ -367,48 +189,7 @@ class Lexer{
 public:
     std::string file_path;
 
-    std::vector<token> lex(){
-	std::vector<token> entire_program;
-
-	scanner->skip_whitespace();
-	
-	while( ! scanner->reached_eof() ){
-	    std::vector<token> output;
-
-	    output = lex_comment();     // try matching comment
-	    if(output == v_empty){
-		output = lex_node();    // try matching node
-	    }
-	    if(output == v_empty){
-		output = lex_edge();    // try matching edge
-	    }
-	    if(output == v_empty){
-		output = lex_group_header(); // try matching group header
-	    }
-	    if(output == v_empty){
-		output = lex_group_close();  // try matching group end
-	    }
-
-	    if(output == v_empty){
-		// nothing could be matched, print error.
-		print_error("Expected group, edge, node or comment");
-		output = v_error;
-	    }
-
-	    if(output == v_error){
-		// an error has occurred while lexing.
-	        // Stop lexing any further.
-		break;
-	    }
-	    
-	    vector_append(entire_program, output);
-
-	    scanner->skip_whitespace();
-	}
-
-
-	return entire_program;
-    }
+    std::vector<token> lex();
     
     Lexer(std::string file_path): file_path(file_path){
 	scanner = new Scanner(file_path);
@@ -416,29 +197,53 @@ public:
     }
 };
 
+std::vector<token> Lexer::lex(){
+    std::vector<token> entire_program;
+
+    scanner->skip_whitespace();
+	
+    while( ! scanner->reached_eof() ){
+	std::vector<token> output;
+
+	output = lex_comment();     // try matching comment
+	if(output == v_empty){
+	    output = lex_node();    // try matching node
+	}
+	if(output == v_empty){
+	    output = lex_edge();    // try matching edge
+	}
+	if(output == v_empty){
+	    output = lex_group_header(); // try matching group header
+	}
+	if(output == v_empty){
+	    output = lex_group_close();  // try matching group end
+	}
+
+	if(output == v_empty){
+	    // nothing could be matched, print error.
+	    print_error("Expected group, edge, node or comment");
+	    output = v_error;
+	}
+
+	if(output == v_error){
+	    // an error has occurred while lexing.
+	    // Stop lexing any further.
+	    break;
+	}
+	
+	vector_append(entire_program, output);
+	
+	scanner->skip_whitespace();
+    }
+
+    return entire_program;
+}
+
 
 int main(){
-
-    /*
-    std::ifstream f ("ipLookup.sg");
-
-    char c;
-    while(true){
-	std::cout<<f.get(c);
-	usleep(10000);
-	if (c == EOF){
-	    std::cout<<"EOF " << EOF <<"\n";
-	}else{
-	    std::cout<<c<<std::flush;
-	}
-    }
-    */
-    
-    
     Lexer l ("ipLookup.sg");
 
     auto result = l.lex();
-    
     
     for(auto t:result){
 	std::cout<< t.first << "\t" << t.second<<"\n";
